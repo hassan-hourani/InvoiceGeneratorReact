184400e4354d72a1a1df0555b91f356904197c8a
hassan-hourani <hassan.j.horani@gmail.com>
2022-05-25 23:06:15 +0300

    h

--- src/App.js
+++ src/App.js
@@ -3,52 +3,20 @@ import mailIcon from "./assets/icons/mailIcon.png"
 function App() {
   const [servicesState, setServicesState] = useState([])
   function handelClick(service) {
-    if(!servicesState.includes(service))
-    return (
-      setServicesState(prev => {
-        return [...prev, service]
-      })
-    )
+    if (!servicesState.includes(service))
+      return (
+        setServicesState(prev => {
+          return [...prev, service]
+        })
+      )
   }
-  // function handelRemove(itemValue) {
-  //   let newArray = [...servicesState]
-  //   let ifCondition = newArray.includes(itemValue)
-  //   newArray.forEach(item => {if(ifCondition)
-  //   return setServicesState(newArray.filter(() => item.value !== itemValue))})
-  // }
-  // function handelRemoveAnchor(index) {
-  //   let indexArray = [...servicesState]
-  //   let indexToBeRemoved = indexArray.indexOf(index)
-  //   indexArray.splice(indexToBeRemoved, 1)
-  //     setServicesState(indexArray)
-  // }
-  function handelRemoveAnchor2(e) {
-    if (servicesState.length === 1) {
-    return setServicesState(servicesState.splice(0,1))
-    }
-    else if (servicesState.length == 2 && e == 0) {
-      return setServicesState(servicesState.splice(0,1))
-      }
-      else if (servicesState.length == 2 && e == 1) {
-        return setServicesState(servicesState.splice(1,1))
-        }
-        else if (servicesState.length == 3 && e == 0) {
-          return setServicesState(servicesState.splice(0,1))
-          }
-          else if (servicesState.length == 3 && e == 1) {
-            return setServicesState(servicesState.splice(1,1))
-            }
-            else if (servicesState.length == 3 && e == 2) {
-              return setServicesState(servicesState.splice(2,1))
-              }
+  function handelRemoveAnchor3(e) {
+    let newArray = [...servicesState]
+    newArray.splice(newArray.indexOf(e), 1)
+    return setServicesState(newArray)
   }
 
-  // function handelRemoveAnchor3() {
-
-  // }
-  
-  const servicesArray = servicesState.map(item => <h4 key={item}>{item}<a onClick={(e) => handelRemoveAnchor2(servicesState.indexOf(item))}>remove</a></h4>)
-  // const servicesArray = servicesState.map(item => <h4 key={item}>{item}<a onClick={(event) => handelRemove(item.value)}>remove</a></h4>)
+  const servicesArray = servicesState.map(item => <h4 key={item}>{item}<a onClick={(e) => handelRemoveAnchor3(item)}>remove</a></h4>)
   const pricesArray = servicesState.map(item => <p key={item}><span>$</span>{item === "Wash Car" ? "10" : item === "Mow Lawn" ? "20" : "30"}</p>)
   return (
     <>
